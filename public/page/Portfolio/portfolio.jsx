2967f9254c80527c6475d6d0eaed780cd9ab1aa8
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-03-30 21:41:28 +0530

    Create Portfolio Asset Table

--- public/page/Portfolio/portfolio.jsx
+++ public/page/Portfolio/portfolio.jsx
@@ -1,9 +1,60 @@
 import React from "react";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Avatar } from "@/components/ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
 
 const Portfolio = () => {
 
     return (
-        <div>Portfolio</div>
+        <div className="p-5 lg:px-10 bg-dark-500  font-bold">
+            <h1 className="font-bold text-3xl pb-5">Portfolio</h1>
+            <Table>
+               <TableHeader>
+                   <TableRow>
+                      <TableHead className="">
+                        Assets
+                      </TableHead>
+                      <TableHead>
+                        Price
+                      </TableHead>
+                      <TableHead>
+                        Unit
+                      </TableHead>
+                      <TableHead>
+                        Change
+                      </TableHead>
+                      <TableHead>
+                        Change%
+                      </TableHead>
+                      <TableHead className="text-right">
+                        Value
+                      </TableHead>
+                   </TableRow>
+              </TableHeader>
+            <TableBody>
+                {[1,1,1,1,1,1,1,1,1,1,1].map((item,index) => <TableRow key={index}>
+                   <TableCell className="font-medium flex items-center gap-2">
+                      <Avatar className="-z-50">
+
+                        <AvatarImage src="https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"/>
+                           
+                      </Avatar>
+                      <span>Bitcoin</span>
+                        
+        
+                    </TableCell>
+                   <TableCell>BTC</TableCell>
+                   <TableCell>32652127595</TableCell>
+                   <TableCell>1660363443420</TableCell>                
+                  <TableCell className="text-right">$83686</TableCell>
+              </TableRow>)}
+              
+            </TableBody> 
+     </Table>
+
+
+
+        </div>
     )
 }
 
--- public/page/home/AssetsTable.jsx
+++ public/page/home/AssetsTable.jsx
@@ -5,49 +5,49 @@ import { AvatarImage } from "@radix-ui/react-avatar";
 
 const AssetTable = () => {
     return(
-     <Table>
-               <TableHeader>
-                   <TableRow>
-                      <TableHead className="w-[100px]">
-                        Coin
-                      </TableHead>
-                      <TableHead>
-                        SYMBOL
-                      </TableHead>
-                      <TableHead>
-                        VOLUME
-                      </TableHead>
-                      <TableHead>
-                        MARKET CAP
-                      </TableHead>
-                      <TableHead>
-                        24h
-                      </TableHead>
-                     <TableHead className="text-right">PRICE</TableHead>
-                   </TableRow>
-              </TableHeader>
-            <TableBody>
-                {[1,1,1,1,1,1,1,1,1,1,1].map((item,index) => <TableRow key={index}>
-                   <TableCell className="font-medium flex items-center gap-2">
-                      <Avatar className="-z-50">
-
-                        <AvatarImage src="https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"/>
-                           
-                      </Avatar>
-                      <span>Bitcoin</span>
-                        
-        
-                    </TableCell>
-                   <TableCell>BTC</TableCell>
-                   <TableCell>32652127595</TableCell>
-                   <TableCell>1660363443420</TableCell>
-                   <TableCell>87724</TableCell>
-
-                  <TableCell className="text-right">$83686</TableCell>
-              </TableRow>)}
-              
-            </TableBody> 
-     </Table>
+    <Table>
+                   <TableHeader>
+                       <TableRow>
+                          <TableHead className="w-[100px]">
+                            Coin
+                          </TableHead>
+                          <TableHead>
+                            SYMBOL
+                          </TableHead>
+                          <TableHead>
+                            VOLUME
+                          </TableHead>
+                          <TableHead>
+                            MARKET CAP
+                          </TableHead>
+                          <TableHead>
+                            24h
+                          </TableHead>
+                         <TableHead className="text-right">PRICE</TableHead>
+                       </TableRow>
+                  </TableHeader>
+                <TableBody>
+                    {[1,1,1,1,1,1,1,1,1,1,1].map((item,index) => <TableRow key={index}>
+                       <TableCell className="font-medium flex items-center gap-2">
+                          <Avatar className="-z-50">
+    
+                            <AvatarImage src="https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"/>
+                               
+                          </Avatar>
+                          <span>Bitcoin</span>
+                            
+            
+                        </TableCell>
+                       <TableCell>BTC</TableCell>
+                       <TableCell>32652127595</TableCell>
+                       <TableCell>1660363443420</TableCell>
+                       <TableCell>87724</TableCell>
+    
+                      <TableCell className="text-right">$83686</TableCell>
+                  </TableRow>)}
+                  
+                </TableBody> 
+         </Table>
 
       
 
