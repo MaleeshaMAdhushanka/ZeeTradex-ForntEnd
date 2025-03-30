4d208cae6a0f69ddea622bac6fd1b26f09b19826
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-03-30 20:12:51 +0530

    Create navigation using Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 
+import { Route, Routes } from 'react-router-dom'
 import Home from '../public/page/home/Home'
 import Navbar from '../public/page/navbar/Navbar'
+import Portfolio from 'public/page/Portfolio/portfolio'
+import Activity from 'public/page/Activity/Activity'
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <Home/>
+      {/* define the alll route */}
+      <Routes>
+        <Route path="/" element={<Home />} />  
+        <Route path="/portfolio" element={<Portfolio />} />  
+        <Route path="/activity" element={<Activity />} />  
+        <Route path="wallet" element={</>} />  
+      
+
+      </Routes>
 
      </>
   )
