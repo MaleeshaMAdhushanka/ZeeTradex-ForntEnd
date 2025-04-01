ee200e6b769404a26fd3a65e31ab3c064b56c410
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-04-02 03:18:28 +0530

    Creat profile Page  AccountVerfication form including twoFac

--- public/page/Profile/Profile.jsx
+++ public/page/Profile/Profile.jsx
@@ -1,7 +1,24 @@
+import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { VerifiedIcon } from "lucide-react";
 import React from "react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
+import AccountVerificationForm from "./AccountVerificationForm";
+  
 
 const Profile = () => {
+
+    const handleEnableTwoStepVerification = () => {
+        console.log("two step verification")
+    }
     return(
         <div className="flex flex-col items-center mb-5">
             <div className="pt-10 w-full lg:w-[70%]">
@@ -65,6 +82,52 @@ const Profile = () => {
                     </CardContent>
 
                 </Card>
+                <div className="mt-6">
+                    <Card className="w-full">
+                        <CardHeader className="pb-7">
+                            <div className="flex items-center gap-3">
+                                <CardTitle>2 Step Verfication</CardTitle>
+
+                               {true ? <Badge className={"space-x-2 text-white bg-green-600"}>
+                                    <VerifiedIcon/>
+                                    <span>
+                                    Enabled
+
+                                    </span>
+                                    
+                                </Badge>: <Badge className="bg-orange-500">Disalbed</Badge>}
+
+                            </div>
+
+
+                        </CardHeader>
+
+                        <CardContent>
+                           <div>
+                            <Dialog>
+                                  <DialogTrigger>
+                                    <Button>Enabled Two Step verification</Button>
+                                  </DialogTrigger>
+                                <DialogContent>
+                                  <DialogHeader>
+                                     <DialogTitle>Verify your account </DialogTitle>
+                                       
+                                   </DialogHeader>
+                                   <AccountVerificationForm handleSubmit={handleEnableTwoStepVerification}/>
+                                   </DialogContent>
+                             </Dialog>
+
+
+                            </div>
+
+                        </CardContent>
+
+
+                    </Card>
+
+
+                </div>
+
 
             </div>
 
