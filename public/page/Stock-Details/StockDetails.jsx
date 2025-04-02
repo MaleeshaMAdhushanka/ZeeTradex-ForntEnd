87b1a857e377b26fdbc5ade40b7c6729fcbdba06
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-04-02 21:16:40 +0530

    Create Stick details  Page coins

--- public/page/Stock-Details/StockDetails.jsx
+++ public/page/Stock-Details/StockDetails.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 const StockDetails = () => {
     return (
-        <div>StockDetails</div>
+        <div className="p-5 mt-5">
+            <div className="flex justify-between">
+                <div className="flex gap-5 items-center">
+                    <div>
+                        <Avatar>
+                            <AvatarImage
+                            src={
+                                "https://coin-images.coingecko.com/coins/images/279/large/ethereum.png?1696501628"
+
+
+                            }
+                            />
+
+                        </Avatar>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
