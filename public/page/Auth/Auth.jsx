0fbf92dae051e12fe44e2d935b8371d46bdb3932
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-04-03 02:52:00 +0530

    Create Auth.css

--- public/page/Auth/Auth.jsx
+++ public/page/Auth/Auth.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-
+import "./Auth.css";
 const Auth = () => {
-    return(
+    return (
         <div className="h-screen relative authContainer">
 
-            <div className="absolute top-0 right-0 left-0 bottom-0 "> 
+            <div className="absolute top-0 left-0 right-0 bottom-0  bg-opacity-100">
 
             </div>
 
         </div>
-    )
-}
+    );
+};
 
 export default Auth;
