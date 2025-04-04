10c1f40ea7fc09cfc3456ff0383cc7334b5e612f
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-04-04 19:31:39 +0530

    Create Signp page

--- public/page/Auth/Auth.jsx
+++ public/page/Auth/Auth.jsx
@@ -4,8 +4,21 @@ const Auth = () => {
     return (
         <div className="h-screen relative authContainer">
 
-            <div className="absolute top-0 left-0 right-0 bottom-0  bg-opacity-100">
 
+
+            <div className="absolute top-0 left-0 right-0 bottom-0  bg-opacity-50">
+                <div className="bg-Blure absolute top-1/2 left-1/2 transform 
+                -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center h-[35rem] w-[35rem] rounded-md z-50 bg-orange-500
+                bg-opacity-50 shadow-2xl shadow-white">
+
+                    <h1 className="text-6xl font-bold pb-9 text-black">Zee Trade X</h1>
+
+                    <section>
+                        
+                    </section>
+
+                </div>
+                 
             </div>
 
         </div>
