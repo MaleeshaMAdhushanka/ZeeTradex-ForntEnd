7ca23efb6ed47d38b7c6882cd45299402cd48d07
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-04-06 01:34:21 +0530

    Create Action.js using axios regiter, login, getUser data method done

---
+++ src/State/Auth/Action.js
@@ -0,0 +1,94 @@
+//use axios library to fetching data form the API
+
+import axios from "axios"
+import { GET_USER_FAILURE, GET_USER_REQUEST, GET_USER_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS } from "./ActionType";
+
+//register method
+
+//geting data from the Server
+//resgiter eka post method ekak nisa
+
+//send action to the store using dispatch
+export const register =(userData) => async(dispatch) =>{
+
+    dispatch({type:REGISTER_REQUEST})
+    
+    const baseUrl = "http://localhost:8080/"
+
+    try {
+      const response = await axios.post(`${baseUrl}/auth/signup`, userData);
+      const user = response.data;
+      console.log(user);
+
+      //register sucess-since we have data
+
+      dispatch({type:REGISTER_SUCCESS, payload:user.jwt})
+
+
+    } catch (error) {
+        dispatch({type:REGISTER_FAILURE, payload:error.response.data.message})
+        console.error(error);
+    }
+
+
+
+}
+//login method
+
+export const login =(userData) => async(dispatch) =>{
+
+    dispatch({type:LOGIN_REQUEST})
+    
+    const baseUrl = "http://localhost:8080/"
+
+    try {
+      const response = await axios.post(`${baseUrl}/auth/signin`, userData);
+      const user = response.data;
+      console.log(user);
+
+      //register sucess-since we have data
+
+      dispatch({type:LOGIN_SUCCESS, payload:user.jwt})
+
+
+    } catch (error) {
+        dispatch({type:LOGIN_FAILURE, payload:error.response.data.message})
+        console.error(error);
+    }
+
+
+
+}
+
+//get user data
+
+export const getUser =(jwt) => async(dispatch) =>{
+
+    dispatch({type:GET_USER_REQUEST})
+    
+    const baseUrl = "http://localhost:8080/"
+
+    try {
+      const response = await axios.get(`${baseUrl}/api/auth/users/profile`,{
+        headers:{
+            Authorization:`Bearer ${jwt}`
+        }
+      });
+      const user = response.data;
+      console.log(user);
+
+      //register sucess-since we have data
+
+      dispatch({type:GET_USER_SUCCESS, payload:user})
+
+
+    } catch (error) {
+        dispatch({type:GET_USER_FAILURE, payload:error.response.data.message})
+        console.error(error);
+    }
+
+
+
+}
+
+
